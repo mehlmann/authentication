581cfa88fea1489eecb8fe34127167bc0301d088
Anton <mehlton@web.de>
2018-01-18 12:22:18 +0100

    Aufgabe und Frage mit Antwort implementiert. Noch nicht getestet.

--- lambda/index.js
+++ lambda/index.js
@@ -1,5 +1,10 @@
 'use strict';
 
+
+
+// globale Variablen
+var sum = 0;
+
 /**
     Diese Funktion baut bei übergebenen Parametern eine Ausgabe für Alexa.
 */
@@ -54,8 +59,8 @@ function getWelcomeResponse(callback) {
 function getHelpResponse(callback) {
     const sessionAttributes = {};
     const cardTitle = 'Hilfe';
-    const speechOutput = 'Du solltest dich authentifizieren.';
-    const repromptText = 'Willst du dich nicht authentifizieren?';
+    const speechOutput = 'Sie sollten sich authentifizieren.';
+    const repromptText = 'Wollen sie sich nicht authentifizieren?';
     const shouldEndSession = false;
 
     callback(sessionAttributes,
@@ -90,6 +95,67 @@ function onSessionEnded(sessionEndedRequest, session, callback) {
     getEndResponse(callback);
 }
 
+function verifyAnswer(intent, callback) {
+    const cardTitle = 'Antwort gegeben';
+    var speechOutput = '';
+    const shouldEndSession = false;
+    try {
+        var answer = intent.slots.antwort.value;
+        speechOutput = `Ihre Anwort war ${answer}`;
+        repromptText = `Ihre Anwort war ${answer}`;
+    } catch (err) {
+        speechOutput = 'Bei Ihrer Antwort ist ein Fehler aufgetreten.';
+        repromptText = 'Bei Ihrer Antwort ist ein Fehler aufgetreten.';
+    }
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function verifyCalc(intent, callback) {
+    const cardTitle = 'Aufgabe gelöst';
+    var speechOutput = '';
+    const repromptText = ``;
+    const shouldEndSession = false;
+    try {
+        var result = intent.slots.loesung.value;
+        speechOutput = `Ihre Lösung war ${result}`;
+        repromptText = `Ihre Lösung war ${result}`;
+    } catch (err) {
+        speechOutput = 'Bei Ihrer Antwort ist ein Fehler aufgetreten.';
+        repromptText = 'Bei Ihrer Antwort ist ein Fehler aufgetreten.';
+    }
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+/**
+ * Stellt dem Benutzer eine simple Additions-Aufgabe.
+ * @param {*} callback 
+ */
+function getCalculation(callback) {
+    var summandA = Math.floor(Math.random() * 20);
+    var summandB = Math.floor(Math.random() * 20);
+    sum = summandA + summandB;
+
+    const cardTitle = 'Aufgabe gestellt';
+    var speechOutput = `Was ist ${summandA} plus ${summandB}?`;
+    const repromptText = `Lösen Sie bitte die Aufgabe ${summandA} plus ${summandB}.`;
+    const shouldEndSession = false;
+
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+/**
+ * Stellt dem Benutzer eine Frage.
+ * @param {*} callback
+ */
+function getQuestion(callback) {
+    const cardTitle = 'Frage gestellt';
+    var speechOutput = `Was ist deine Lieblingsfarbe?`;
+    const repromptText = `Beantworte mir, was deine Lieblingsfarbe ist.`;
+    const shouldEndSession = false;
+
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
 /**
  * Ruft die verschiedenen intents auf.
  * @param {*} intentRequest 
@@ -97,14 +163,19 @@ function onSessionEnded(sessionEndedRequest, session, callback) {
  * @param {*} callback 
  */
 function onIntent(intentRequest, session, callback) {
-    const intentName = intentRequest.intent.name;
-
-    if (intentName === 'AMAZON.HelpIntent') {
-        getHelpResponse(callback);
-    } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
+    const intent = intentRequest.intent;
+    if (intent.name === 'AMAZON.StopIntent' || intent.name === 'AMAZON.CancelIntent') {
         getEndResponse(callback);
-    } else if (intentName === 'LaunchRequest') {
-        getWelcomeResponse(callback);
+    } else if (intent.name === 'AMAZON.HelpIntent') {
+        getHelpResponse(callback);
+    } else if (intent.name === 'Antworten') {
+        verifyAnswer(intent, callback);
+    } else if (intent.name === 'Rechenloesung') {
+        verifyCalc(intent, callback);
+    } else if (intent.name === 'Rechenaufgabe') {
+        getCalculation(callback);
+    } else if (intent.name === 'Frage') {
+        getQuestion(callback);
     } else {
         throw new Error('Invalid intent');
     }
