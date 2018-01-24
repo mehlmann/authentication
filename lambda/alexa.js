f0a62ec9285bd9f2462b8744a9fc3f1ed4f77634
Anton <mehlton@web.de>
2018-01-24 18:17:06 +0100

    npm-install of javascript-state-machine

--- lambda/alexa.js
+++ lambda/alexa.js
@@ -1,18 +1,35 @@
-var StateMachine = require('javascript-state-machine')
+/**
+ * Der Verlauf einer Authentifizierung kann als State-Machine gesehen werden. 
+ * Dies soll die Variable alexa_state implementieren.
+ * Ein Beispiel einer Fragen-State-Machine könnte sein:
+ * 
+ *  ------   Antwort korrekt   ------   Antwort korrekt   -------
+ * |quest1|------------------>|quest2|------------------>|authen.|
+ *  ------                     ------                     -------
+ *    |                           |
+ *    |                           v Antwort falsch
+ *    |      Antwort falsch    -------
+ *    |---------------------->| abort |
+ *                             -------
+ */
+
+var StateMachine = require('./includes/javascript-state-machine')
 
 
 var alexa_state = new StateMachine({
-    init = 'start',
+    init : 'quest1',
     transitions: [
-        { name: 'answer', from: 'start', to: 'end'},
-        { name: 'again',    from: 'end', to: 'start'}
+        { name: 'answer_correct', from: 'quest1', to: 'quest2'},
+        { name: 'answer_correct', from: 'quest2', to: 'authenticated'},
+        { name: 'answer_wrong',    from: 'quest1', to: 'abort'},
+        { name: 'answer_wrong',    from: 'quest2', to: 'abort'}
     ],
     methods: {
-        onAnswer: function() {
-
+        onAnswerCorrect: function() {
+            console.log('Answer was correct.');
         },
-        onAgain:  function() {
-
+        onAnswerWrong:  function() {
+            console.log('Answer was wrong!');
         }     
     }
 });
